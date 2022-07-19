4a68f9daf40191fd352cc50513d0e9dbcc1d78a3
heroineyy <93510710@qq.com>
2022-07-20 00:54:00 +0800

    first commit

---
+++ etcd_lease_demo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"log"
+	"time"
+)
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second * 5,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connect to etcd success.")
+	defer cli.Close()
+
+	// 创建一个5秒的租约
+	resp, err := cli.Grant(context.TODO(), 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 5秒钟之后, /nazha/ 这个key就会被移除
+	_, err = cli.Put(context.TODO(), "/nazha/", "dsb", clientv3.WithLease(resp.ID))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
